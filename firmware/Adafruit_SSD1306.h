0b3b50790236311a8ae7e68002582925ee76106a
Patrick Walsh <callovarne@gmail.com>
2015-12-07 16:04:36 -0800

    Fixing includes

--- firmware/Adafruit_SSD1306.h
+++ firmware/Adafruit_SSD1306.h
@@ -17,7 +17,7 @@ All text above, and the splash screen must be included in any redistribution
 *********************************************************************/
 
 #include "application.h"
-#include "Adafruit_GFX.h"
+#include "Adafruit_GFX/Adafruit_GFX.h"
 
 #define BLACK 0
 #define WHITE 1
